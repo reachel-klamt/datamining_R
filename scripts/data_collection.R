00b217c46ad6be2301893534b299c14920862960
reachel-klamt <132921688+reachel-klamt@users.noreply.github.com>
2026-03-07 12:26:03 +0100

    added data folders to git ignore and updated api request

--- scripts/data_collection.R
+++ scripts/data_collection.R
@@ -6,15 +6,26 @@
   library(httr2)
   library(jsonlite)
   library(tidyverse)
+  library(here)
 
 # fetch data from the internal API
-  response <- request("https://icsid.worldbank.org/api/all/cases") |>
+  http_response <- request("https://icsid.worldbank.org/api/all/cases") |>
     req_perform()
   
-# extract the cases from the response
-  cases_list <- resp_body_string(response) |>
+# extract the content of the response as text
+  json_response <- http_response |> 
+    resp_body_string()
+  
+# save the raw response as json file
+  writeLines(json_response, here("data_raw", "icsid_raw.json"))
+
     
-# convert the JSON response to a data frame (list)
-    fromJSON(simplifyVector = FALSE)  |>
+# convert the JSON response to an R list
+  cases_list <- json_response |>
+    fromJSON(simplifyVector = FALSE) |>
     {\(x) x$data$GetAllCasesResult}()
+  
+# create vector with all case names
+
+
   
